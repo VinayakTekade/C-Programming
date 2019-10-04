4b3ee546a31b6dacf130436a7d4022924fd06500
VinayakTekade <vinayak.tekade@gmail.com>
2019-10-04 14:42:33 +0530

    Added factorial

---
+++ Frequently asked C programs/factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+    int i, f, num;
+    char ch = 'y';
+    do
+    {
+        f = 1;
+        printf("Enter a number\n");
+        scanf("%d", &num);
+
+        for (i = 1; i <= num; i++)
+            f = f * i;
+
+        printf("Factorial of %d is: %d \n", num, f);
+
+        printf("Do you want to continue?(y/n):");
+        scanf(" %c", &ch);
+    } while ((ch == 'Y') || (ch == 'y'));
+}
